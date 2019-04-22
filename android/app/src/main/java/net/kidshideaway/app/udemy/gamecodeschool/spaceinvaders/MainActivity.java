194193c62a897a573d8b27445faeed4146780fb7
Erica and Keit Southworth <elsandkls@kidshideaway.net>
2019-04-22 17:41:32 -0400

    reorganized the repository
    Deleted the old repository and reogranized it under a new one.  There's a directory for each language or platform specific code base.  I took a Udemy class in one language and I'm porting the example game that was built in the class to multiple languages. The goal being to main consistency between the versions.

---
+++ android/app/src/main/java/net/kidshideaway/app/udemy/gamecodeschool/spaceinvaders/MainActivity.java
@@ -0,0 +1,16 @@
+package net.kidshideaway.app.udemy.gamecodeschool.spaceinvaders;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+
+// SpaceInvadersActivity is the entry point to the game.
+// It will handle the lifecycle of the game by calling
+// methods of spaceInvadersEngine when prompted to so by the OS.
+public class  MainActivity  {
+
+    // spaceInvadersView will be the view of the game
+    // It will also hold the logic of the game
+    // and respond to screen touches as well
+}
